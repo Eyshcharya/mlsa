eab461404adbd2b887e635fe78b4d459ac2f7380
Eyshcharya Chandrasekara <eyshcharya@gmail.com>
2024-05-13 16:42:28 +0000

    chore: updated profile data

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Eyshcharya Chandrasekara",
+  title: "Associate Software Engineer",
+  email: "eyshcharya@gmail.com",
+  gitHub: "https://github.com/Eyshcharya",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
+  linkedIn: "https://www.linkedin.com/in/eyshcharya/",
+  medium: "https://medium.com/@eyshcharya",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
